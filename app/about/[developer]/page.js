8de2d63563089763da98d4a3359d411cab0a2740
akaAditya <mymail.adityapatel@gmail.com>
2023-09-24 23:56:09 +0530

    fourth commit final about-us page

--- app/about/[developer]/page.js
+++ app/about/[developer]/page.js
@@ -14,32 +14,26 @@ const Developer = () => {
     { id: 2, name: "Vaibhav", role: "Backend Developer" },
     { id: 3, name: "Suresh", role: "Frontend Developer" },
   ];
+  const developer = details.map((data) => {
+    if (path === "/about/1") {
+      if (data.id === 1) {
+        return `${data.name} - ${data.role}`
+      }
+    } else if (path === "/about/2") {
+      if (data.id === 2) {
+        return `${data.name} - ${data.role}`
+      }
+    } else if (path === "/about/3") {
+      if (data.id === 3) {
+        return `${data.name} - ${data.role}`
+      }
+    } else{
+      return "Developer doesn't exist";
+    }
+  })
   return (
     <div>
-      <h1>{name}</h1>
-      {details.map((data) => {
-        if (path === "/about/1") {
-          if (data.id === 1) {
-            <h1>
-              {data.name}, {data.role}
-            </h1>;
-          }
-        } else if (path === "/about/2") {
-          if (data.id === 2) {
-            <h1>
-              {data.name}, {data.role}
-            </h1>;
-          }
-        } else if (path === "/about/3") {
-          if (data.id === 3) {
-            <h1>
-              {data.name}, {data.role}
-            </h1>;
-          }
-        } else {
-          <h2>{"Developer doesn't exist"}</h2>;
-        }
-      })}
+      <h1>{developer}</h1>
     </div>
   );
 };
--- app/about/page.js
+++ app/about/page.js
@@ -1,7 +1,20 @@
-"use client"
+"use client";
+
+import Link from "next/link";
+import React from "react";
+
 // /about
 
 const AboutUsPage = () => {
-    return <h1>About-Us Page</h1>;
-  };
-  export default AboutUsPage;
+  return (
+    <React.Fragment>
+      <h1>About-Us Page</h1>
+      <ul>
+        <li><Link href='/about/1'>Yash</Link></li>
+        <li><Link href='/about/2'>Vaibhav</Link></li>
+        <li><Link href='/about/3'>Suresh</Link></li>
+      </ul>
+    </React.Fragment>
+  );
+};
+export default AboutUsPage;
